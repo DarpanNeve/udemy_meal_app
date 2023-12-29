c5942e93c73e1f091b1b11439e8520cbfc790cd2
Darpan <darpanneve3@gmail.com>
2023-12-29 11:57:46 +0530

    Animation Added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -4,19 +4,57 @@ import 'package:meal_app/widgets/category_grid_item.dart';
 
 import '../models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
-final List<Meal> availableMeals;
+
+  final List<Meal> availableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 300),
+        lowerBound: 0,
+        upperBound: 1);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  GridView(
-          padding: const EdgeInsets.all(16),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 20, mainAxisSpacing: 20),
-          children: [
-            for (final i in availableCategories)
-              CategoryGridItem(category: i, availableMeals: availableMeals,)
-          ],
-        );
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 20, mainAxisSpacing: 20),
+        children: [
+          for (final i in availableCategories)
+            CategoryGridItem(
+              category: i,
+              availableMeals: widget.availableMeals,
+            )
+        ],
+      ),
+      builder: (ctx, child) => Padding(
+          padding: EdgeInsets.only(top:100- _animationController.value * 100),
+          child: child),
+    );
   }
 }
